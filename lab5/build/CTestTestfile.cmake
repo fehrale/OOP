8e37f7517ec773e8a2d293681a543a79bda81edb
Kate <144523523+fehrale@users.noreply.github.com>
2024-12-11 16:34:25 +0300

    Delete lab5 directory

--- lab5/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/OOP1/lab5
-# Build directory: /workspaces/OOP1/lab5/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(MyProjectTests "/workspaces/OOP1/lab5/build/tests")
-set_tests_properties(MyProjectTests PROPERTIES  _BACKTRACE_TRIPLES "/workspaces/OOP1/lab5/CMakeLists.txt;39;add_test;/workspaces/OOP1/lab5/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
