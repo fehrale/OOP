15c0a41345b42d6399054d0fa2c07ff29c771317
Kate <144523523+fehrale@users.noreply.github.com>
2024-12-15 02:35:07 +0300

    Update factory.h

--- lab7/include/factory.h
+++ lab7/include/factory.h
@@ -3,7 +3,7 @@
 #include <memory>
 #include <istream>
 #include <string>
-#include "npc.h" // Заголовочный файл с объявлением NPC и типов NpcType
+#include "npc.h"
 
 std::shared_ptr<NPC> factory(std::istream& is);
 std::shared_ptr<NPC> factory(NpcType type, int x, int y);
